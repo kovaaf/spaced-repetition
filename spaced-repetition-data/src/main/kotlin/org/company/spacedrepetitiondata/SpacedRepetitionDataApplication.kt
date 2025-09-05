b9c8753b2c305be5bf4fa17081778c5760ad6847
Anton Kovalyov <kovalyov.af7@gmail.com>
2025-09-05 17:10:44 +0700

    Add data module

---
+++ spaced-repetition-data/src/main/kotlin/org/company/spacedrepetitiondata/SpacedRepetitionDataApplication.kt
@@ -0,0 +1,11 @@
+ package org.company.spacedrepetitiondata
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpacedRepetitionDataApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpacedRepetitionDataApplication>(*args)
+}
---
+++ spaced-repetition-data/src/test/kotlin/org/company/spacedrepetitiondata/TestEmptyForJacoco.kt
@@ -0,0 +1,13 @@
+package org.company.spacedrepetitiondata
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestEmptyForJacoco {
+
+    @Test
+    fun emptyTestMethodForJacoco() {
+    }
+
+}
